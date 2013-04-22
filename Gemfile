b6c6e9ecb4e23ae99fed57c82aad6abc0f04c408
Dave Aronson <github2dave@davearonson.com>
2013-04-22 10:56:03 -0400

    some work on adding omniauth

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0.beta1'
 
+gem 'omniauth'
+gem 'omniauth-github'
 gem 'pg'
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ PullRequestRoulette::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  root to: 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
