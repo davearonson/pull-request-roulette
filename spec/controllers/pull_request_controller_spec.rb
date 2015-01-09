aca1fef82c79111c70faa6658c1d891f5d91c5be
Dave Aronson <github.2.trex@codosaur.us>
2015-01-09 15:40:37 -0500

    dry up spec

--- spec/controllers/pull_request_controller_spec.rb
+++ spec/controllers/pull_request_controller_spec.rb
@@ -3,23 +3,27 @@ require "ostruct"
 
 describe PullRequestsController do
 
-  before do
-    set_up_faked_github_oauth
-  end
+  describe "requires authorization" do
 
-  it "requires authorization to get the form to submit a pr" do
-    get :new
-    expect(response).to redirect_to(@github_auth_url)
-  end
+    before do
+      set_up_faked_github_oauth
+    end
 
-  it "requires authorization to submit a pr" do
-    post :create
-    expect(response).to redirect_to(@github_auth_url)
-  end
+    it "to get the form to submit a pr" do
+      get :new
+      expect(response).to redirect_to(@github_auth_url)
+    end
 
-  it "requires authorization to take a pr" do
-    post :take, pull_request_id: 1
-    expect(response).to redirect_to(@github_auth_url)
+    it "to submit a pr" do
+      post :create
+      expect(response).to redirect_to(@github_auth_url)
+    end
+
+    it "to take a pr" do
+      post :take, pull_request_id: 1
+      expect(response).to redirect_to(@github_auth_url)
+    end
+
   end
 
 end
